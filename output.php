8971164c13d2e971433a2072b316c6f87c373257
Jesse Petersen <jesse@petersenmediagroup.com>
2013-01-09 10:30:40 -0500

    changed Line 26 to "featured-image" class
    for CSS consistency with theme defaults

--- output.php
+++ output.php
@@ -23,7 +23,7 @@ function gmie_banner_image() {
 	elseif ( is_singular( array( 'post', 'page' ) ) ) {
 	$custom_image = genesis_get_custom_field( '_gmie_image' );
 		if ( $custom_image ) {
-		echo '<img class="custom-image " src="' . esc_html( $custom_image ) . '" alt="' . esc_html( $image_alt ) . '" />';
+		echo '<img class="featured-image" src="' . esc_html( $custom_image ) . '" alt="' . esc_html( $image_alt ) . '" />';
 		}
 	}
 }
