a983bd6adf1842b02924fc3adbbb0b4afb305946
Jesse Petersen <jesse@petersenmediagroup.com>
2013-01-08 22:04:33 -0500

    "banner" for consistency and removed post images
    Replaced instances of referring to it as the featured image with "banner" image.
    Removed all of the code for inserting either custom or featured image into the post content - that is to be done manually by the author to retain full editorial control.

--- output.php
+++ output.php
@@ -1,9 +1,9 @@
 <?php
 
 /** Load everything */
-add_action( 'genesis_meta', 'load_gmie_featured_image' );
+add_action( 'genesis_meta', 'load_gmie_banner_image' );
 
-function load_gmie_featured_image() {
+function load_gmie_banner_image() {
 
 	/** Remove default minimum image if it exists */
 	if ( function_exists('minimum_featured_image') ) {
@@ -15,56 +15,12 @@ function load_gmie_featured_image() {
 }
 
 /** Include the featured image */
-function gmie_featured_image() {
+function gmie_banner_image() {
 	if ( is_home() ) {
 		echo '<div id="featured-image"><img src="'. get_stylesheet_directory_uri() . '/images/sample.jpg" /></div>';
 	}
 	
 	elseif ( is_singular( array( 'post', 'page' ) ) ) {
-		gmie_display_featured();
-	}
-}
-
-/** Include the custom image on posts & pages */
-function gmie_custom_image() {
-	if ( is_singular( array( 'post', 'page' ) ) ) {
-		gmie_display_custom();
-	}
-}
-
-/** Display the featured image if it exists */
-function gmie_display_featured() {
-	if ( has_post_thumbnail() ) {
-		echo '<div id="featured-image">' . get_the_post_thumbnail($post->ID, 'header') . '</div>';
-	}
-}
-
-/** Display the custom image if it exists */
-function gmie_display_custom() {
-		
-	// Check to see if alignment has been set
-	function image_align_check() {
-		$get_alignment = genesis_get_custom_field( '_gmie_align' );
-		
-		if ( $get_alignment ) {
-			return $get_alignment;
-		}
-		else {
-			return 'alignright';
-		}
-	}
-
-	$image_align = image_align_check();
-	$custom_image = genesis_get_custom_field( '_gmie_image' );
-	$image_alt = get_the_title();
-
-	if ( $custom_image ) {
-		echo '<img class="custom-image ' . esc_html( $image_align ) . '" src="' . esc_html( $custom_image ) . '" alt="' . esc_html( $image_alt ) . '" />';
-	}
-
-	else {
-		if ( has_post_thumbnail() ) {
-			echo the_post_thumbnail( 'gmie-custom', array('class' => 'custom-image ' . esc_html( $image_align ) . '', 'alt'	=> esc_html( $image_alt ) ) );
-		}
+		gmie_display_banner();
 	}
 }
